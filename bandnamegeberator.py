e38fef33b2684d560947e58d0aac5f2211b1adb3
gungunkhilrani <gungunkhilrani446@gmail.com>
2024-09-06 22:39:15 +0530

    Initial commit

---
+++ bandnamegeberator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("Enter The Name Of The City You Grew Up In : ")
+pet = input("Enter Your Pet's Name : ")
+print("This Could Be Your Band Name : " + city + " " + pet)
